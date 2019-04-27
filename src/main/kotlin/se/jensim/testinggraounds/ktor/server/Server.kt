83223c10b53a2cc49770665de3905814dfb5006f
Jens Brimfors <jens.brimfors@nordnet.se>
2019-04-27 12:47:55 +0200

    Created a switch for serving static files from src instead of classpath, in order run angular in build --watch mode and have instant reload

--- src/main/kotlin/se/jensim/testinggraounds/ktor/server/Server.kt
+++ src/main/kotlin/se/jensim/testinggraounds/ktor/server/Server.kt
@@ -2,18 +2,25 @@
 
 package se.jensim.testinggraounds.ktor.server
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.application.Application
+import io.ktor.config.HoconApplicationConfig
 import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.util.KtorExperimentalAPI
 import se.jensim.testinggraounds.ktor.server.config.install
 import se.jensim.testinggraounds.ktor.server.config.root
 
+@KtorExperimentalAPI
 fun main(args: Array<String>) {
-    embeddedServer(Netty, port = 8080, module = Application::mainModule).start(wait = true)
+    val conf = HoconApplicationConfig(ConfigFactory.load())
+    val port = conf.config("ktor.deployment").property("port").getString().toInt()
+    embeddedServer(Netty, port = port, module = Application::mainModule).start(wait = true)
 }
 
+@KtorExperimentalAPI
 fun Application.mainModule() {
     install()
     routing {
--- src/main/kotlin/se/jensim/testinggraounds/ktor/server/config/ServerRoutes.kt
+++ src/main/kotlin/se/jensim/testinggraounds/ktor/server/config/ServerRoutes.kt
@@ -1,28 +1,31 @@
 package se.jensim.testinggraounds.ktor.server.config
 
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
 import io.ktor.http.content.default
 import io.ktor.http.content.files
 import io.ktor.http.content.static
 import io.ktor.http.content.staticRootFolder
 import io.ktor.routing.Route
-import io.ktor.routing.Routing
 import io.ktor.routing.route
-import org.slf4j.LoggerFactory
+import io.ktor.util.KtorExperimentalAPI
 import se.jensim.testinggraounds.ktor.server.healthcheck.healthcheck
 import java.io.File
 
+@KtorExperimentalAPI
 fun Route.root() {
-    val log = LoggerFactory.getLogger(Routing::class.java)
     route("api") {
         healthcheck()
     }
     static("/") {
-        staticRootFolder = File(javaClass.getResource("/static").toURI())
-        /*
-         TODO
-         Make able to handle env dev for serving from file system, and not classpath
-         println("staticRootFolder=$staticRootFolder")
-         */
+        val conf = HoconApplicationConfig(ConfigFactory.load())
+        val env = conf.config("ktor.deployment").property("environment").getString()
+        staticRootFolder = if (env == "production") {
+            File(javaClass.getResource("/frontend").toURI())
+        } else {
+            // Assume dev mode
+            File("src/main/resources/frontend")
+        }
         files(".")
         default("index.html")
     }
