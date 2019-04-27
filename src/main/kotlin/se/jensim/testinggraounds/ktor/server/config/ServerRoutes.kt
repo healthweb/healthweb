45d3499addc7bbbf9f992775b6faebd64e07e068
Jens Brimfors <jens.brimfors@nordnet.se>
2019-04-27 07:26:43 +0200

    Added a basic static route

--- src/main/kotlin/se/jensim/testinggraounds/ktor/server/config/ServerRoutes.kt
+++ src/main/kotlin/se/jensim/testinggraounds/ktor/server/config/ServerRoutes.kt
@@ -1,24 +1,30 @@
 package se.jensim.testinggraounds.ktor.server.config
 
-import io.ktor.application.call
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
+import io.ktor.http.content.default
+import io.ktor.http.content.files
+import io.ktor.http.content.static
+import io.ktor.http.content.staticRootFolder
 import io.ktor.routing.Route
 import io.ktor.routing.Routing
-import io.ktor.routing.get
 import io.ktor.routing.route
 import org.slf4j.LoggerFactory
-import se.jensim.testinggraounds.ktor.server.healthcheck.Healthcheck
 import se.jensim.testinggraounds.ktor.server.healthcheck.healthcheck
+import java.io.File
 
 fun Route.root() {
     val log = LoggerFactory.getLogger(Routing::class.java)
-    get("/") {
-        log.info("Responding..!!")
-        call.respond(HttpStatusCode.OK, Healthcheck("HEJ"))
-    }
     route("api") {
         healthcheck()
+    }
+    static("/") {
+        staticRootFolder = File(javaClass.getResource("/static").toURI())
+        /*
+         TODO
+         Make able to handle env dev for serving from file system, and not classpath
+         println("staticRootFolder=$staticRootFolder")
+         */
+        files(".")
+        default("index.html")
     }
     healthcheck()
 }
