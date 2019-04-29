8f5493195a4dadf21932cf56e5126a303f9b1dfc
Jens Brimfors <jens.brimfors@nordnet.se>
2019-04-29 09:22:48 +0200

    Trying to reuse the same objectMapper if possible, not to create a million with the same conf.

--- src/main/kotlin/se/jensim/testinggraounds/ktor/server/config/ObjectMapperConfig.kt
+++ src/main/kotlin/se/jensim/testinggraounds/ktor/server/config/ObjectMapperConfig.kt
@@ -7,11 +7,21 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.util.concurrent.atomic.AtomicReference
 
 object ObjectMapperConfig {
 
-    fun config(objectMapper: ObjectMapper? = null) =
-        objectMapper ?: jacksonObjectMapper().run {
+    private val ob: AtomicReference<ObjectMapper> = AtomicReference()
+
+    fun config(objectMapper: ObjectMapper? = null): ObjectMapper =
+        if (objectMapper != null) {
+            ob.compareAndSet(null, objectMapper)
+            objectMapper
+        } else {
+            val mapper = jacksonObjectMapper()
+            ob.compareAndSet(null, mapper)
+            mapper
+        }.run {
             configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
             configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             configure(DeserializationFeature.FAIL_ON_TRAILING_TOKENS, true)
