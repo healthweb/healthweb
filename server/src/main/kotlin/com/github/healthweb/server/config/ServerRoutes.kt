a2812f7a0d8793a48247b91b36543b5aa0ba247e
Jens Brimfors <jens.brimfors@nordnet.se>
2019-07-26 11:29:04 +0200

    Docker (H2) now works in some fashion

--- server/src/main/kotlin/com/github/healthweb/server/config/ServerRoutes.kt
+++ server/src/main/kotlin/com/github/healthweb/server/config/ServerRoutes.kt
@@ -1,12 +1,12 @@
 package com.github.healthweb.server.config
 
+import com.github.healthweb.server.healthcheck.healthcheck
 import io.ktor.http.content.default
 import io.ktor.http.content.files
 import io.ktor.http.content.static
 import io.ktor.http.content.staticRootFolder
 import io.ktor.routing.Route
 import io.ktor.routing.route
-import com.github.healthweb.server.healthcheck.healthcheck
 import java.io.File
 
 fun Route.root() {
@@ -15,7 +15,7 @@ fun Route.root() {
     }
     static("/") {
         staticRootFolder = if (PropertiesConfig.isProd()) {
-            File(javaClass.getResource("/frontend").toURI())
+            File("/frontend") // Docker assumed path
         } else {
             // Assume dev mode
             File("../frontend/dist")
