0e14490bcbb3210c4687c5acba6782a30d05d2ba
Jens Brimfors <jens.brimfors@nordnet.se>
2019-07-30 06:27:48 +0200

    Clean up docker H2 solution

--- docker/build_docker-mysql.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -v
-
-docker build -f docker/Dockerfile-mysql -t healthweb-h2 .
--- docker/h2/build.sh
+++ docker/h2/build.sh
@@ -3,5 +3,5 @@
 set -e
 set -v
 
-docker build -f docker/Dockerfile-h2 -t healthweb/healthweb:h2 .
+docker build -t healthweb/healthweb:h2 .
 docker push healthweb/healthweb:h2
