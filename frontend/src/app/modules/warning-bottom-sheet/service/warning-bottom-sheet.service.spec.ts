3ddc6e94656f97a0baaa4382d47145d0866fd62d
Jens Brimfors <jens.brimfors@nordnet.se>
2019-08-13 13:24:35 +0200

    Import necessary module

--- frontend/src/app/modules/warning-bottom-sheet/service/warning-bottom-sheet.service.spec.ts
+++ frontend/src/app/modules/warning-bottom-sheet/service/warning-bottom-sheet.service.spec.ts
@@ -1,9 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {WarningBottomSheetService} from './warning-bottom-sheet.service';
+import {MatBottomSheetModule} from "@angular/material";
 
 describe('WarningBottomSheetService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      MatBottomSheetModule
+    ],
+  }));
 
   it('should be created', () => {
     const service: WarningBottomSheetService = TestBed.get(WarningBottomSheetService);
